fc0551f69885d11fd0581c47b007c95209af9867
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:35 +0530

    Remove comments part 52/100

--- program11.sh
+++ program11.sh
@@ -12,14 +12,8 @@ do
 
     counter=$(( counter + 1 ))
 
-    # Syntax: variable=$(echo "expression" | bc)
-    # - echo "$ans * $counter" prints the expression as a string
-    # - | (pipe) sends it to bc which evaluates it
-    # We use bc instead of $((...)) because bc handles arbitrarily large integers
-    # Shell arithmetic would overflow for factorials larger than 20! (> 2^63)
     ans=$(echo "$ans * $counter" | bc)
 
-# done - marks the end of the while loop
 # When counter equals fact, the condition [ $fact -ne $counter ] becomes false
 # and loop exits with 'ans' holding the final factorial value
 done
