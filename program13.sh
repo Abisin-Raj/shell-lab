a8afacf26de38d533663a02f038a8e027cb8f3e4
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:34 +0530

    Remove comments part 33/100

--- program13.sh
+++ program13.sh
@@ -16,15 +16,9 @@ do
     sum=$((sum + i))
 done
 
-# We echo the arithmetic string "scale=2; $sum / $n" into bc
 avg=$(echo "scale=2;$sum/$n" | bc)
 
-# Display the calculated average
 echo "Average of the numbers = $avg"
 
 
-# Output:
-# __________________________________________
-# | $ ./program13.sh 10 20 30              |
-# | Average of the numbers = 20.00         |
 # |________________________________________|
