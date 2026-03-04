57714dc65693fb96383c4146df620e7e292ef3ca
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:35 +0530

    Remove comments part 64/100

--- program2.sh
+++ program2.sh
@@ -50,11 +50,6 @@ fi
 
 done
 
-# Output:
-# __________________________________________
-# | MENU                                   |
-# | 1. Addition                            |
-# | 2. Subtraction                         |
 # | 3. Multiplication                      |
 # | 4. Division                            |
 # | Enter your choice                      |
