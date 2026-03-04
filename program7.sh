42430997090bcd7ac2302fd91aa6ca4179522f52
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:34 +0530

    Remove comments part 39/100

--- program7.sh
+++ program7.sh
@@ -26,16 +26,10 @@ z=$((x + y))
 
 echo "$z"
 
-# This prepares x for the next calculation
 x=$y
 
-# Move z (current term) to y (previous term)
-# This prepares y for the next calculation
-# After this: x and y are ready for next Fibonacci number
 y=$z
 
-# done - marks end of while loop
-# Example progression for n=7:
 #   Initial: x=0, y=1 (printed)
 #   Iter 1: z=0+1=1, x=1, y=1
 #   Iter 2: z=1+1=2, x=1, y=2
