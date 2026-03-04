2c74e5ec8246e00d28b8f96e7489bbb6de1af365
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:34 +0530

    Remove comments part 46/100

--- program14.sh
+++ program14.sh
@@ -7,17 +7,11 @@ echo "Current time: $now"
 
 now=$(date +"%H")
 
-#   -le : less than or equal to    (<=)
-#   -gt : greater than             (>)
-#   -a  : logical AND - both conditions must be true
 
-# if branch: hour is between 06 (6 AM) and 12 (noon) inclusive -> Morning
-# [ $now -ge 06 -a $now -le 12 ] means: (hour >= 6) AND (hour <= 12)
 if [ $now -ge 06 -a $now -le 12 ]
 then
     echo "Good morning"
 
-# elif branch: hour is between 13 (1 PM) and 17 (5 PM) inclusive -> Afternoon
 # [ $now -gt 12 -a $now -le 17 ] means: (hour > 12) AND (hour <= 17)
 elif [ $now -gt 12 -a $now -le 17 ]
 then
