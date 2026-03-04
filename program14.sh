947dd6387140b81677c4921e7a4e4b201271a4c0
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:34 +0530

    Remove comments part 47/100

--- program14.sh
+++ program14.sh
@@ -12,19 +12,13 @@ if [ $now -ge 06 -a $now -le 12 ]
 then
     echo "Good morning"
 
-# [ $now -gt 12 -a $now -le 17 ] means: (hour > 12) AND (hour <= 17)
 elif [ $now -gt 12 -a $now -le 17 ]
 then
     echo "Good afternoon"
 
-# else branch: catches all other hours (18:00 - 05:59) -> Evening/Night
 else
     echo "Good evening"
-# fi - ends the if/elif/else block (if spelled backwards)
 fi
 
-# Output:
-# __________________________________________
-# | Current time: 13:00:00                 |
 # | Good afternoon                         |
 # |________________________________________|
