3a1f9318890e7b47a50b7dedec5c3a3532c32aba
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:34 +0530

    Remove comments part 15/100

--- program1.sh
+++ program1.sh
@@ -9,16 +9,10 @@ area=$(echo "3.14 * $r * $r" | bc)
 
 circum=$(echo "3.14 * 2 * $r" | bc)
 
-# $area is expanded to its calculated value within the double-quoted string
 echo "Area of the circle is: $area"
 
-# Similar to above - prints the circumference with variable expansion
-# $circum is replaced with the calculated circumference value
 echo "Circumference of the circle is: $circum"
 
-# Output:
-# __________________________________________
-# | Enter the radius of the circle:        |
 # | 5                                      |
 # | Area of the circle is: 78.5            |
 # | Circumference of the circle is: 31.4   |
