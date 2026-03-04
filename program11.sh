2243a016deb6697fdd03adde9c6cf33d3560caab
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:35 +0530

    Remove comments part 53/100

--- program11.sh
+++ program11.sh
@@ -14,16 +14,10 @@ do
 
     ans=$(echo "$ans * $counter" | bc)
 
-# When counter equals fact, the condition [ $fact -ne $counter ] becomes false
-# and loop exits with 'ans' holding the final factorial value
 done
 
-# echo with variable interpolation - prints the computed factorial
-# $fact is the original input, $ans is the computed n!
 echo "Factorial of $fact is $ans"
 
-# Output:
-# __________________________________________
 # | Factorial of the number?               |
 # | 5                                      |
 # | Factorial of 5 is 120                  |
