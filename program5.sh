3f9f84ff28d6c1a696f01af52e2fc8285db6cd27
ABISIN RAJ <abisin@example.com>
2026-03-05 05:24:34 +0530

    Remove comments part 4/100

--- program5.sh
+++ program5.sh
@@ -14,14 +14,8 @@ do
 
 sd=$((n % 10))
 
-# Formula: rev = rev * 10 + sd
-# Multiply current rev by 10 to shift digits left
-# Add the extracted digit to the right
-# Example progression for 12345:
-#   rev=0*10+5=5, rev=5*10+4=54, rev=54*10+3=543, etc.
 rev=$((rev * 10 + sd))
 
-# Remove the rightmost digit from n
 # / is integer division operator (truncates decimal)
 # Example: 12345 / 10 = 1234 (removes last digit)
 # This prepares n for the next iteration
